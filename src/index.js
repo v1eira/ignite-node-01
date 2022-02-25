fd1540c146f36f947a96e67c6cb034b1bf11c0ef
Ewerson Vieira <ewersonv@mobilesaude.com.br>
2022-02-25 02:16:32 -0300

    Fix retorno de checkExistsUserAccount

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ function checksExistsUserAccount(request, response, next) {
   const user = users.find(user => user.username === username);
 
   if (!user) {
-    return response.status(400).json({ error: "User not found" });
+    return response.status(404).json({ error: "User not found" });
   }
 
   request.user = user;
